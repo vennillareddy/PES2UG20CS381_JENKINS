7f257f4977719eb70f577d2e95393c4eaa5c05b6
vennillareddy <116337899+vennillareddy@users.noreply.github.com>
2023-02-18 16:01:45 +0530

    Add files via upload

---
+++ pes2ug20cs381_t5.cpp
@@ -0,0 +1,12 @@
+//File: pes2ug20cs381_t5.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Universe\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG20CS381\n";
+  cout << "Successful Pipeline\n";
+  cout << "Bye\n";
+  return 0;
+}
