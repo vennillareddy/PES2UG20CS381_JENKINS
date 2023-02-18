b4e3ebb91a66946c04fd453d3f82dee54ae8081c
vennillareddy <116337899+vennillareddy@users.noreply.github.com>
2023-02-18 15:52:50 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, Galaxy\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully built jenkins\n";
   return 0;
 }
